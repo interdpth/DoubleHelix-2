be6f794e6ce1637536ffa7a6747e3cc1055f04ae
interdpth <interdpth@gmail.com>
2021-03-10 18:18:16 -0600

    Fixed most poses on samus editor, need to fix a few specific frames,got export working.

--- uiSamusEditorClass.cpp
+++ uiSamusEditorClass.cpp
@@ -24,37 +24,37 @@ int SamusEditorClass::SetupSamusPreview(SamusBase* base)
 	long size = 0;
 	int i = 0;
 	int ii = 0;
-	unsigned char* compBuffer = new unsigned char[64691];
-	GBAMethods* GBA = new GBAMethods();
+	//unsigned char* compBuffer = new unsigned char[64691];
+	//GBAMethods* GBA = new GBAMethods();
 
 
-	unsigned char* decompbuf = new unsigned char[32687];
+	//unsigned char* decompbuf = new unsigned char[32687];
 
-	unsigned short transferpal[256] = { 0 };
+	//unsigned short transferpal[256] = { 0 };
 
 	int  X = 0;
 	long off = 0;
 	int x = 0;
-
-
-	GBA->DecodePal((short*)base->Suit_color, SpritePreview->PreviewPal, 16, 0);
-
-	memcpy(SpritePreview->PreRAM, base->Sprite_tiles, 0x8000);
-
-	SpritePreview->OAM.clear();
-	//SpritePreview->maxparts = 0;
+	SpritePreview = base->theSprite;
+//
+//	GBA->DecodePal((short*)base->Suit_color, SpritePreview->PreviewPal, 16, 0);
+//
+//	memcpy(SpritePreview->PreRAM, base->Sprite_tiles, 0x8000);
+//
 //	SpritePreview->OAM.clear();
-	i = 0;
-	for (i = 0; i < base->theSprite->maxparts; i++) SpritePreview->OAM.push_back(base->theSprite->OAM[i]);
-	SpritePreview->maxparts = i;
-
-	SpritePreview->id = 0xFF;
-
-	//RD1Engine::theGame->mgrOAM->DecodeOAM(GBA->ROM, true, SpritePreview,(unsigned long) base->SamusOAMPointer - 0x8000000);
-	SpritePreview->PreviewSprite.GetFullImage()->Clear();
-	RD1Engine::theGame->mgrOAM->DrawPSprite(SpritePreview);
-
-	delete[] compBuffer;
-	delete[] decompbuf;
+//	//SpritePreview->maxparts = 0;
+////	SpritePreview->OAM.clear();
+//	i = 0;
+//	for (i = 0; i < base->theSprite->maxparts; i++) SpritePreview->OAM.push_back(base->theSprite->OAM[i]);
+//	SpritePreview->maxparts = i;
+//
+//	SpritePreview->id = 0xFF;
+//
+//	//RD1Engine::theGame->mgrOAM->DecodeOAM(GBA->ROM, true, SpritePreview,(unsigned long) base->SamusOAMPointer - 0x8000000);
+//	SpritePreview->PreviewSprite.GetFullImage()->Clear();
+//	RD1Engine::theGame->mgrOAM->DrawPSprite(SpritePreview);
+//
+//	delete[] compBuffer;
+//	delete[] decompbuf;
 	return 0;
 }
